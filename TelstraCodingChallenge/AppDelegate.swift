358e1a975b19e23d943bf461d2c753d36e0683e5
Noble Mathew <noby.matty@gmail.com>
2020-12-04 08:02:24 +1100

    Initial commit

---
+++ TelstraCodingChallenge/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  TelstraCodingChallenge
+//
+//  Created by Mathew, Noble (Cognizant) on 4/12/20.
+//  Copyright © 2020 Mathew, Noble. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        true
+    }
+}
---
+++ TelstraCodingChallengeTests/TelstraCodingChallengeTests.swift
@@ -0,0 +1,33 @@
+//
+//  TelstraCodingChallengeTests.swift
+//  TelstraCodingChallengeTests
+//
+//  Created by Mathew, Noble (Cognizant) on 4/12/20.
+//  Copyright © 2020 Mathew, Noble. All rights reserved.
+//
+
+@testable import TelstraCodingChallenge
+import XCTest
+
+class TelstraCodingChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
